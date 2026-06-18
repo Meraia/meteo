f9e292898f322f25a2ee9f04bf87ec417a44a269
Marie Storkova <mariestorkova1@gmail.com>
2026-06-17 21:46:43 -0700

    added java script file

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+
+}
+
+
+
+let searchFormElement = document.querySelector("#search-form");
+console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
