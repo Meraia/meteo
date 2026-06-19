cd26b12b1feb0215e57b02e61a61d414946f078d
Marie Storkova <mariestorkova1@gmail.com>
2026-06-18 19:01:01 -0700

    added search engine

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-console.log(searchInput.value);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
 }
 
 
